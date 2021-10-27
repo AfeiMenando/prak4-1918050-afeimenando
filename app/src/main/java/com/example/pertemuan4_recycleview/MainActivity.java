6817d9d7326f7f881dab4d8612521e951c842573
Afei MenandO <mynameisnando23@gmail.com>
2021-10-27 21:05:16 +0700

    first commit

--- app/src/main/java/com/example/pertemuan4_recycleview/MainActivity.java
+++ app/src/main/java/com/example/pertemuan4_recycleview/MainActivity.java
@@ -10,30 +10,29 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
-    private MahasiswaAdapter adapter;
-    private ArrayList<Mahasiswa> mahasiswaArrayList;
+    private itemAdapter adapter;
+    private ArrayList<item> itemArrayList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addData();
-        recyclerView =
-                (RecyclerView)findViewById(R.id.recycleview);
-        adapter = new MahasiswaAdapter(mahasiswaArrayList);
-        RecyclerView.LayoutManager layoutManager = new
-                LinearLayoutManager(MainActivity.this);
+        recyclerView = (RecyclerView) findViewById(R.id.recycleview);
+        adapter = new itemAdapter(itemArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
     }
+
     private void addData() {
-        mahasiswaArrayList = new ArrayList<>();
-        mahasiswaArrayList.add(new Mahasiswa("Ahmad", "1918001",
-                "2019"));
-        mahasiswaArrayList.add(new Mahasiswa("Yusuf", "1918002",
-                "2019"));
-        mahasiswaArrayList.add(new Mahasiswa("Akbar", "1918003",
-                "2019"));
+        itemArrayList = new ArrayList<>();
+        itemArrayList.add(new item("VOTKA", "BIG", "2",R.drawable.min_votka));
+        itemArrayList.add(new item("ORANG TUA", "BIG", "5",R.drawable.min_ot));
+        itemArrayList.add(new item("JACK DANIELS", "BIG", "3",R.drawable.min_jd));
+        itemArrayList.add(new item("BRANDY", "SMALL", "4",R.drawable.min_brandy));
+        itemArrayList.add(new item("SMIRNOFF", "SMALL", "5",R.drawable.min_smirnoff));
+        itemArrayList.add(new item("WHISKY", "BIG ", "2",R.drawable.min_whisky));
+        itemArrayList.add(new item("VIBE", "SMALL", "3",R.drawable.min_vibe));
     }
 }
-
-
